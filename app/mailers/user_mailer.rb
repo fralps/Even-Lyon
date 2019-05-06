75bbbd2f03ce3f6544b0869faf320b2af6103b07
CaroFG <carolina.ferreira131@gmail.com>
2019-05-06 14:56:27 +0200

    mailer almost done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,13 @@ class UserMailer < ApplicationMailer
 		mail(to: @user.email, subject: 'Bienvenue sur Event Lyon')
 
 	end
+
+	def event_welcome_email(event)
+		admin_id = event.admin_id
+		@admin = User.find(admin_id)
+		@url = 'https://event-lyon.herokuapp.com/event/attendance'
+
+		mail(to: @admin.email, subject: 'Nouveau participant')
+
+	end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,13 @@
 class Attendance < ApplicationRecord
+	after_create :welcome_event
 	belongs_to :user
 	belongs_to :event
 
 	#validates :stripe_customer_id, uniqueness: true
+
+	def welcome_event
+		event_id = self.event_id
+		event = Event.find(event_id)
+		UserMailer.event_welcome_email(event).deliver_now
+	end
 end
