d9237d4389acff62638b6f23e50ef903cb3bbe8f
fralps <francois.loupias@orange.fr>
2019-05-07 16:01:58 +0200

    WIP almost done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+  	@event = Event.new
+  end
+
+  def create
+  	@event = Event.new(admin: current_user, start_date: params[:start_date], duration: params[:duration], title: params[:title], description: params[:description], price: params[:price], location: params[:location])
+		@event.save
+
+		if @event.save
+			redirect_to root_path
+			flash[:success] = "Your event has been successfully created"
+		else
+			render 'new'
+		end
+  end
+
+  def show  	
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [:show]
+
+  def show
+  	dont_show
+  	@user = User.find(params[:id])
+  end
+
+  def dont_show
+  	@user = User.find(params[:id])
+  	if @user != current_user
+  		redirect_to root_path
+  		flash[:alert] = "Condidential Information"
+  	end
+  end
+end
